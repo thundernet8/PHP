e0b57b8fc903056f133594f37626cd07139108a9
吴学谦 <wuxueqian@ezbuy.com>
2017-12-15 14:47:03 +0800

    add collection

---
+++ collection/build.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var path = require("path");
+var ejs = require("ejs");
+var showdown = require("showdown");
+var converter = new showdown.Converter();
+
+var md = fs.readFileSync(path.resolve("./index.md")).toString();
+var template = fs.readFileSync(path.resolve("./template.ejs")).toString();
+
+try {
+    var html = ejs.render(template, { content: converter.makeHtml(md) });
+    fs.writeFileSync(path.resolve("./index.html"), html);
+} catch (err) {
+    throw err;
+}
---
+++ collection/index.md
@@ -0,0 +1,7 @@
+## Web Best Practices
+
+* [Google WebFundamentals](https://developers.google.com/web/fundamentals) - [Github](https://github.com/google/WebFundamentals)
+
+## Animation
+
+* [GSAP (GreenSock Animation Platform)](https://greensock.com/) - [Github](https://github.com/greensock/GreenSock-JS)
