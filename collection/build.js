938c0d335448028aeb24fd8da5e380bbe35ba2d7
吴学谦 <wuxueqian@ezbuy.com>
2017-12-15 18:01:37 +0800

    update

--- collection/build.js
+++ collection/build.js
@@ -4,12 +4,14 @@ var ejs = require("ejs");
 var showdown = require("showdown");
 showdown.setFlavor("github");
 var converter = new showdown.Converter();
+var moment = require("moment");
 
 var md = fs.readFileSync(path.resolve("./index.md")).toString();
 var template = fs.readFileSync(path.resolve("./template.ejs")).toString();
 
 try {
-    var html = ejs.render(template, { content: converter.makeHtml(md) });
+    var data = { content: converter.makeHtml(md), version: moment().format("YYYYMMDDHHmmss") };
+    var html = ejs.render(template, data);
     fs.writeFileSync(path.resolve("./index.html"), html);
 } catch (err) {
     throw err;
